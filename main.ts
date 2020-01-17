d650e7411f37f588052773c2ca15de4bce708460
jiangfuzhi <jfuzhi@163.com>
2020-01-17 15:27:25 +0800

    Create a module

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
+//test
 
+//% weight=10 color=#9F79EE icon="\uf108" block="Light"
+namespace light {
+    //% block
+    export function Led_On() {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    }
+
+}
