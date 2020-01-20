00e044028ea48f2784754ef8e00aba9872040533
jiangfuzhi <jfuzhi@163.com>
2020-01-20 16:27:38 +0800

    add Led_On block

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 
 //% weight=10 color=#9F79EE icon="\uf108" block="Light"
 namespace light {
-    //% block
+    //% blockId=ligth_led_on block="Led_On"
     export function Led_On() {
         pins.digitalWritePin(DigitalPin.P2, 1)
     }
