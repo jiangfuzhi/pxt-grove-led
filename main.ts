50915fc2ee395a95f320cddd308c847c9da6bf81
jiangfuzhi <jfuzhi@163.com>
2020-01-20 17:28:53 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 
 //% weight=10 color=#9F79EE icon="\uf108" block="Light"
 namespace light {
-    //% blockId=ligth_led_on block="Led_On"
+    //% block
     export function Led_On() {
         pins.digitalWritePin(DigitalPin.P2, 1)
     }
