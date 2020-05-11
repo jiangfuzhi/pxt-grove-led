ef42a7edd6e43071352964593126a6b361b9d68f
jiangfuzhi <jfuzhi@163.com>
2020-05-10 19:37:06 -0700

    add led_off

--- main.ts
+++ main.ts
@@ -2,9 +2,14 @@
 
 //% weight=10 color=#9F79EE icon="\uf108" block="Light"
 namespace light {
-    //% block
+    //% blockID = led_on
+    //% weight=40
     export function Led_On() {
         pins.digitalWritePin(DigitalPin.P2, 1)
     }
-
+    //% blockID = led_off
+    //% weight=30
+    export function Led_Off(): void {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
 }
